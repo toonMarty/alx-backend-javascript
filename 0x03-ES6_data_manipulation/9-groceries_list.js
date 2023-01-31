c86fa1d0f6a8cc2c1365fd19e7ec57922024e4f3
toonMarty <martinnyaga7373@gmail.com>
2023-01-31 13:16:54 -0800

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const fruitMap = new Map();
+  fruitMap.set('Apples', 10);
+  fruitMap.set('Tomatoes', 10);
+  fruitMap.set('Pasta', 1);
+  fruitMap.set('Rice', 1);
+  fruitMap.set('Banana', 5);
+  return fruitMap;
+}
