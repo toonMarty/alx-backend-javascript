d720f8ab568c1f00f7ce94ad7b9b086514b04ba2
toonMarty <martinnyaga7373@gmail.com>
2023-01-31 11:02:49 -0800

    Range check

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new RangeError('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const int8View = new DataView(buffer, 0);
   int8View.setInt8(position, value);
